59927eaaea85d2f180437fcd90a2761672a059fa
thesagardahiwal <133154948+thesagardahiwal@users.noreply.github.com>
2023-08-21 09:51:32 +0530

    added new project

---
+++ javaScript.js
@@ -0,0 +1,30 @@
+let url = "https://catfact.ninja/fact";
+
+async function getFact(){
+    try{
+        let res = await axios.get(url);
+         return (res.data.fact);
+        
+    }catch(e){
+        console.log("error is ",e);
+    }
+}
+
+
+let btn = document.querySelector('button');
+let para = document.querySelector('p');
+
+btn.addEventListener('click', async ()=>{
+    let fact = await getFact();
+    para.innerText = fact;
+})
+
+// async function getFact(url){
+//     try{let res = await fetch(url);
+//         let data = await res.json();
+//         console.log(data.fact);
+//     }catch(e){
+//         console.log("error is ",e);
+//     }
+//     console.log("finished!")
+// }
